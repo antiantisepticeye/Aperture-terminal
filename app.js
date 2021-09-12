6d058d9c1d2af4c9c9c2fbc1856119a2f780b612
antiantisepticeye <antiantisepticeye666@gmail.com>
2021-09-12 22:50:19 +0530

    Added commands

---
+++ app.js
@@ -0,0 +1,36 @@
+function startup() {
+    $('.initialize').remove()
+    setInterval(() => {
+        $('.text-area_').terminal({
+            cat: async function() {
+                this.pause()
+                response = await fetch("https://api.thecatapi.com/v1/images/search")
+                cat = await response.json()
+                console.log(cat)
+                var img=new Image();
+                    
+                // console.log(responseData.imageData)
+                img.src=cat[0].url;
+                img.onload = () => { this.resume(); console.log('resumed') }
+                img.classList.add("terminal-image")
+
+
+                image = $(img)
+                this.echo(image)
+            },
+            hello: function() {
+                
+            }
+        }, {
+            checkArity: false,
+            greetings: `Anti's Terminal v0.0.1.\n`,
+            completion: false,
+            convertLinks:false,
+            prompt:"$ "
+        })
+    }, 500)        
+}
+
+
+setInterval(startup, Math.floor(Math.random() *3000))
+
